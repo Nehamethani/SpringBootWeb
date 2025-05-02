0cc418b8ece662cc1b3b9679792bbd08c7222204
Neha Methani <nehamethani54@gmail.com>
2025-05-02 23:37:09 +0530

    ModelAndView

--- src/main/java/com/nekocodes/SpringBootWeb/HomeController.java
+++ src/main/java/com/nekocodes/SpringBootWeb/HomeController.java
@@ -4,10 +4,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.boot.web.servlet.server.Jsp;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -22,16 +24,21 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(@RequestParam("num1") int n1, @RequestParam("num2") int n2, HttpSession session)
+    public ModelAndView add(@RequestParam("num1") int n1, @RequestParam("num2") int n2, ModelAndView mv)
     {
 
 //        int n1 = Integer.parseInt(req.getParameter("num1"));
 //        int n2 = Integer.parseInt(req.getParameter("num2"));
         int result = n1+n2;
 
-        session.setAttribute("result", result);
-        System.out.println(result);
-        return "result";
+//        MV.addAttribute("result",result);
+//        System.out.println(result);
+//        return "result";
+
+          mv.addObject("result", result);
+          mv.setViewName("result");
+
+          return mv;
     }
 
 }
